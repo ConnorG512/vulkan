b1d3d0c77dcbd9b345637c61f7440d94c408ef3a
Connor Garey <connorgarey1999@gmail.com>
2026-03-13 21:55:20 +0000

    command_buffer_begin_info

--- include/vulkan/initializer.hpp
+++ include/vulkan/initializer.hpp
@@ -16,4 +16,5 @@ namespace Vulkan
 
   auto fence_create_info(VkFenceCreateFlags flags = 0) -> VkFenceCreateInfo;
   auto semaphore_create_info(VkSemaphoreCreateFlags flags = 0) -> VkSemaphoreCreateInfo;
+  auto command_buffer_begin_info(VkCommandBufferUsageFlags flags = 0) -> VkCommandBufferBeginInfo;
 }
--- src/vulkan/initializer.cpp
+++ src/vulkan/initializer.cpp
@@ -13,12 +13,25 @@ auto Vulkan::fence_create_info(VkFenceCreateFlags flags) -> VkFenceCreateInfo
 
   return info;
 }
+
 auto Vulkan::semaphore_create_info(VkSemaphoreCreateFlags flags) -> VkSemaphoreCreateInfo
 {
   VkSemaphoreCreateInfo info {
     .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
     .pNext = nullptr,
     .flags = flags,
+  };
+  return info;
+}
+
+auto command_buffer_begin_info(VkCommandBufferUsageFlags flags) -> VkCommandBufferBeginInfo
+{
+  VkCommandBufferBeginInfo info = 
+  {
+    .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+    .pNext = nullptr,
+    .flags = flags,
+    .pInheritanceInfo = nullptr,
   };
   return info;
 }
