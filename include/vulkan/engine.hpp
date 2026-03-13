517c64dad6fe40dbda8d13a8df21d4b0a98b8ff7
Connor Garey <connorgarey1999@gmail.com>
2026-03-13 17:08:47 +0000

    Set up init

--- include/vulkan/engine.hpp
+++ include/vulkan/engine.hpp
@@ -1,6 +1,9 @@
 #pragma once 
 
+#include <memory>
 #include <vulkan/vulkan_core.h>
+#include <SDL3/SDL_video.h>
+
 namespace Vulkan 
 {
   class Engine 
@@ -12,6 +15,11 @@ namespace Vulkan
       VkDevice device;
       VkSurfaceKHR suface;
 
+      std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window {nullptr, nullptr};
+
+      bool is_initialised = false;
+
+      auto init() -> void;
       auto init_vulkan() -> void;
       auto init_swapchain() -> void;
       auto init_commands() -> void;
--- src/vulkan/engine.cpp
+++ src/vulkan/engine.cpp
@@ -1,5 +1,33 @@
 #include "vulkan/engine.hpp"
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_video.h>
+#include <format>
+#include <stdexcept>
+
+constexpr bool use_validation_layers {false};
+
+auto Vulkan::Engine::init() -> void 
+{
+  if(!SDL_Init(SDL_INIT_VIDEO))
+    throw std::runtime_error(std::format("Failed to init video. [{}],", SDL_GetError()));
+
+  SDL_WindowFlags window_flags {SDL_WINDOW_VULKAN};
+
+  window.reset(SDL_CreateWindow("window", 1280, 720, window_flags));
+  if(window == nullptr)
+    throw std::runtime_error(std::format("Failed to create window. [{}],", SDL_GetError()));
+
+  init_vulkan();
+  init_swapchain();
+  init_commands();
+  init_sync_structures();
+
+  is_initialised = true;
+}
+
 auto Vulkan::Engine::init_vulkan() -> void 
 {
 
