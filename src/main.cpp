ee3cecb0cfa3475ea9f085f1171df285580ef35e
Connor Garey <connorgarey1999@gmail.com>
2026-03-14 12:52:18 +0000

    Added SDL Loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include "vulkan/engine.hpp"
+#include <SDL3/SDL_events.h>
 
 auto main() -> int 
 {
   Vulkan::Engine engine{};
   engine.init();
-  engine.draw();
+  
+  bool done {false};
+
+  while(!done)
+  {
+    SDL_Event event; 
+
+    while (SDL_PollEvent(&event)) 
+    {
+      if(event.type == SDL_EVENT_QUIT)
+        done = true;
+    }
+
+    engine.draw();
+  }
+
   return 0;
 }
