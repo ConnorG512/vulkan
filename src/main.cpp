9951bd402d8ba4f0e34d073a9a5d4fcf51149e58
Connor Garey <connorgarey1999@gmail.com>
2026-03-14 14:42:21 +0000

    Added SDL init

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "vulkan/engine.hpp"
+#include "window.hpp"
+
 #include <SDL3/SDL_events.h>
+#include <SDL3/SDL.h>
+#include <format>
+#include <stdexcept>
+
 
 auto main() -> int 
 {
+  if(!SDL_Init(SDL_INIT_VIDEO))
+    throw std::runtime_error(std::format("Failed to init video. [{}],", SDL_GetError()));
+  Window::Instance window{};
+
   Vulkan::Engine engine{};
   engine.init();
   
