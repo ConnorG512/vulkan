5e68b3a12b12cfabe6c3c0c1b2a4cb296d5000d8
Connor Garey <connorgarey1999@gmail.com>
2026-03-18 18:00:04 +0000

    Create compute pipeline and layout factories.

--- include/vulkan/pipeline.hpp
+++ include/vulkan/pipeline.hpp
@@ -1,7 +1,14 @@
+#include <cstdint>
 #include <vulkan/vulkan_core.h>
-#pragma one 
+#pragma one
 
-namespace Vulkan::Pipeline 
-{
-  auto create_compute_pipeline(VkShaderModule computeShader, VkPipelineLayout pipelineLayout, const char *pName = "main") noexcept -> VkComputePipelineCreateInfo;
-}
+namespace Vulkan::Pipeline {
+auto create_compute_pipeline(VkShaderModule computeShader,
+                             VkPipelineLayout pipelineLayout,
+                             const char *pName = "main") noexcept
+    -> VkComputePipelineCreateInfo;
+
+auto create_pipeline_layout_info(const VkDescriptorSetLayout &setLayout,
+                                 std::uint32_t setLayoutCount = 1) noexcept
+    -> VkPipelineLayoutCreateInfo;
+} // namespace Vulkan::Pipeline
--- src/vulkan/pipeline.cpp
+++ src/vulkan/pipeline.cpp
@@ -1,19 +1,31 @@
 #include "vulkan/pipeline.hpp"
+#include <cassert>
 
-auto Vulkan::Pipeline::create_compute_pipeline(VkShaderModule computeShader, VkPipelineLayout pipelineLayout, const char *pName) noexcept -> VkComputePipelineCreateInfo
-{
-  VkPipelineShaderStageCreateInfo stageInfo {
-    .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-    .pNext = nullptr,
-    .stage = VK_SHADER_STAGE_COMPUTE_BIT,
-    .module = computeShader,
-    .pName = pName,
+auto Vulkan::Pipeline::create_compute_pipeline(VkShaderModule computeShader,
+                                               VkPipelineLayout pipelineLayout,
+                                               const char *pName) noexcept
+    -> VkComputePipelineCreateInfo {
+  return VkComputePipelineCreateInfo{
+      .sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
+      .pNext = nullptr,
+      .stage = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .pNext = nullptr,
+        .stage = VK_SHADER_STAGE_COMPUTE_BIT,
+        .module = computeShader,
+        .pName = pName,
+      },
+      .layout = pipelineLayout,
   };
+}
 
-  return VkComputePipelineCreateInfo {
-    .sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
-    .pNext = nullptr,
-    .stage = stageInfo,
-    .layout = pipelineLayout,
+auto Vulkan::Pipeline::create_pipeline_layout_info(
+    const VkDescriptorSetLayout &setLayout,
+    std::uint32_t setLayoutCount) noexcept -> VkPipelineLayoutCreateInfo {
+  return VkPipelineLayoutCreateInfo{
+      .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+      .pNext = nullptr,
+      .setLayoutCount = setLayoutCount,
+      .pSetLayouts = &setLayout,
   };
 }
