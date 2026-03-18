3ddd2808e9a5119460ebf1315ff7a0d798883739
Connor Garey <connorgarey1999@gmail.com>
2026-03-18 15:12:46 +0000

    Remove auto explicit return

--- include/file-util.hpp
+++ include/file-util.hpp
@@ -2,7 +2,7 @@
 
 #include <fstream>
 namespace Util {
-inline constexpr auto get_file_size_from_end = [](std::ifstream &file) -> auto {
+inline constexpr auto get_file_size_from_end = [](std::ifstream &file) {
   return file.tellg();
 };
 } // namespace Util
