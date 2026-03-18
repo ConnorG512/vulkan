877216f0e72824f1d148bd16e0ac72be7569d420
Connor Garey <connorgarey1999@gmail.com>
2026-03-18 15:48:17 +0000

    get_shader_file_path
    .

--- include/file-util.hpp
+++ include/file-util.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
+#include <cstdlib>
+#include <filesystem>
+#include <format>
 #include <fstream>
+#include <string>
 namespace Util {
 inline constexpr auto get_file_size_from_end = [](std::ifstream &file) {
   return file.tellg();
+};
+
+inline constexpr auto get_shader_file_path =
+    [](const char *shader_name) -> std::string {
+  const char *shader_path{std::getenv("SHADER_PATH")};
+  return (shader_path != nullptr)
+             ? std::format("{}/shaders/{}", shader_path, shader_name)
+             : std::string{
+                   std::format("{}/shaders/{}",
+                               std::filesystem::canonical("/proc/self/exe")
+                                   .parent_path()
+                                   .c_str(),
+                               shader_name)};
 };
 } // namespace Util
