de506dc76147ace27a7b6c01b4f9b1b470749824
Connor Garey <connorgarey1999@gmail.com>
2026-03-22 16:26:49 +0000

    Created Rendering file

---
+++ src/vulkan/rendering.cpp
@@ -0,0 +1,27 @@
+#include <vulkan/rendering.hpp>
+
+auto Vulkan::Rendering::create_rendering_attachment_info(
+    const RenderingAttachmentSettings &renderingAttachmentSettings)
+    -> VkRenderingAttachmentInfo {
+  const VkRenderingAttachmentInfo result{
+      .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO,
+      .pNext = renderingAttachmentSettings.pNext,
+      .imageView = renderingAttachmentSettings.imageView,
+      .imageLayout = renderingAttachmentSettings.imageLayout,
+      .resolveMode = renderingAttachmentSettings.resolveMode,
+      .resolveImageView = renderingAttachmentSettings.resolveImageView,
+      .resolveImageLayout = renderingAttachmentSettings.resolveImageLayout,
+      .loadOp = renderingAttachmentSettings.loadOp,
+      .storeOp = renderingAttachmentSettings.storeOp,
+      .clearValue = renderingAttachmentSettings.clearValue,
+  };
+
+  return result;
+}
+
+auto Vulkan::Rendering::create_rendering_info() -> VkRenderingInfo {
+  const VkRenderingInfo result{
+
+  };
+  return result;
+}
